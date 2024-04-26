c6f9053efdd14dbbac08a38ceb15637e27d9623d
SopitaNipona (Diego) <a01748692@tec.mx>
2024-04-26 16:53:19 -0600

    fixed dynamo

--- src/services/dynamodbService.ts
+++ src/services/dynamodbService.ts
@@ -1,2 +1,11 @@
 import dynamodb from 'dynamodb';
 import { AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN } from '../config/index';
+
+dynamodb.AWS.config.update({
+    region: AWS_REGION,
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    sessionToken: AWS_SESSION_TOKEN
+});
+
+export default dynamodb;
