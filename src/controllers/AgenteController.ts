e140b42ecd3feb9719d31e16aad40d49f8be53d0
SopitaNipona (Diego) <a01748692@tec.mx>
2024-04-26 16:03:25 -0600

    Update AgenteController.ts

--- src/controllers/AgenteController.ts
+++ src/controllers/AgenteController.ts
@@ -1,10 +1,11 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import AbstractController from "./AbstractController";
-import db from "../models";
+import db from "../models"
 
-class AgenteController extends AbstractController{
-    //Singleton
-    //Atributos de clase
+class AgenteController extends AbstractController {
+
+    // Singleton
+    // Atributos de clase
     private static _instance: AgenteController;
     public static get instance():AgenteController{
         if(this._instance){
@@ -12,37 +13,51 @@ class AgenteController extends AbstractController{
         }
         this._instance = new AgenteController("agente");
         return this._instance;
-    }   
-
-    protected initializeRoutes(): void {
-        this.router.get("/test",this.getTest.bind(this));
-        //CRUD
-        //this.router.get("/consultar",);
-        this.router.post("/crear",this.postCrear.bind(this));
-        //this.router.post("/cambiar",);
-        //this.router.post("/eliminar",);       
     }
 
-    private async postCrear(req: Request, res: Response){
+    protected initializeRoutes(): void {
+        this.router.get("/test", this.getTest.bind(this));
+
+        // CRUD
+        this.router.get("/consultar", this.getConsultar.bind(this));
+        this.router.post("/crear", this.postCrear.bind(this));
+        // this.router.post("/cambiar",);
+        // this.router.post("/eliminar",);
+    }
+
+    private async postCrear(req: Request, res: Response) {
         try{
             console.log(req.body);
             await db.Agente.create(req.body);
-            console.log("Agente creado")
+            console.log("Agente creado");
             res.status(200).send("Agente creado");
         }catch(err){
-            console.error(err);
-            res.status(500).send("Error al crear agente");
+
         }
     }
+
+    private async getConsultar(req:Request, res: Response){
+        try {
+            console.log("Consultar agentes");
+            let agentes = await db["Agente"].findAll();
+            res.status(200).json(agentes);
+        }
+        catch(err){
+            console.error(err);
+            res.status(500).send("Error mijooo");
+        }
+    }
+
     private async getTest(req: Request, res: Response){
         try{
             console.log("AgenteController works");
             res.status(200).send("AgenteController works");
         }catch(err){
             console.error(err);
-            res.status(500).send("Error en AgenteController");
+            res.status(500).send
         }
     }
+    
 }
 
 export default AgenteController;
